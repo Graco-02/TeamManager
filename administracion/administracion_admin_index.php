9a2bf24e76c4f3459fd86c380c9b27d64c1b2168
graco <fferrer02@hotmail.com>
2024-06-25 11:58:30 -0300

    cambios visuales al inicio

--- administracion/administracion_admin_index.php
+++ administracion/administracion_admin_index.php
@@ -10,6 +10,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/admin_css.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        
         <title>administracion_principal</title>
     </head>
     <body>
@@ -20,14 +23,23 @@
                     <div class="logo_name">
                         <p>Team Manager</p>
                     </div>
-                </div>
-                <div class="menu_administracion_principal">
+
+                    <div class="menu_administracion_principal">
                     <a href="../usuarios/admin_usuarios.php">usuarios</a> 
                     <a href="../eventos/administracion_eventos.php">eventos</a> 
                     <a href="../equipos/administracion_equipos.php">equipos</a> 
                     <a href="../jugadores/administracion_jugadores.php">jugadores</a> 
                     <a href="../histlog/histlog.php">historico</a> 
+                    </div>
+
                 </div>
+                <!--<div class="menu_administracion_principal">
+                    <a href="../usuarios/admin_usuarios.php">usuarios</a> 
+                    <a href="../eventos/administracion_eventos.php">eventos</a> 
+                    <a href="../equipos/administracion_equipos.php">equipos</a> 
+                    <a href="../jugadores/administracion_jugadores.php">jugadores</a> 
+                    <a href="../histlog/histlog.php">historico</a> 
+                </div>-->
             </div>
         </header>
     </body>
