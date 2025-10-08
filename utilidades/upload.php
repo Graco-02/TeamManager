252bf11a35aa1c62ec05cc3fe11168a403416ead
graco <fferrer02@hotmail.com>
2025-10-08 12:01:45 -0400

    se corrige subida de imagenes se agrega ramdon al final del nombre para evitar machar por nombres rpetidos al subir

--- utilidades/upload.php
+++ utilidades/upload.php
@@ -1,9 +1,23 @@
 <?php 
-   
+
+
    if (!file_exists('imagenes_subidas')) {
       mkdir('imagenes_subidas', 0777);
     }
 
-    move_uploaded_file($_FILES['file']['tmp_name'], '../imagenes_subidas/'. $_FILES['file']['name']);
-    echo $_FILES['file']['name'];
+    $randomString = generateRandomString(15); 
+
+    move_uploaded_file($_FILES['file']['tmp_name'], '../imagenes_subidas/'. $_FILES['file']['name'].$randomString);
+    echo $_FILES['file']['name'].$randomString;
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 ?>
--- utilidades/upload2.php
+++ utilidades/upload2.php
@@ -4,6 +4,19 @@
       mkdir('archivos_subidos', 0777);
     }
 
-    move_uploaded_file($_FILES['file']['tmp_name'], '../archivos_subidos/'. $_FILES['file']['name']);
-    echo $_FILES['file']['name'];
+    $randomString = generateRandomString(15); 
+
+    move_uploaded_file($_FILES['file']['tmp_name'], '../archivos_subidos/'. $_FILES['file']['name'].$randomString);
+    echo $_FILES['file']['name'].$randomString;
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 ?>
